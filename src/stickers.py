61c4957be2b20f7d181ea6d30b4e90f8555a3958
Angelo Lima <angeloantonio442@gmail.com>
2022-08-02 13:59:20 -0300

    Update stickers.py

--- src/stickers.py
+++ src/stickers.py
@@ -1,13 +1,25 @@
 import requests
 import os
+from PIL import Image
+from io import BytesIO
+import re
 
 request = requests.get("https://mocki.io/v1/9a7c1ca9-29b4-4eb3-8306-1adb9d159060").json()
 
-filmes = request["items"]
+films = request["items"]
 
-for filme in filmes:
-    image = requests.get(filme['image']).content
-    try: os.makedirs('stickers')
-    except: pass
-    with open(os.path.join("stickers", filme['title'] + ".jpg"), "wb") as img:
-        img.write(image)
+try:os.makedirs('stickers')
+except FileExistsError: pass
+
+def save(film):
+    response = requests.get(film['image'])
+    if response.status_code == 200:
+        title = re.sub(r'[^\w]', '_', film['title']) + ".jpg"
+        image = Image.open(BytesIO(response.content))
+        image.thumbnail((512,512))
+        image.save(optimize=True, quality=100, fp=os.path.join('stickers', title))
+    else:
+        print("Error on ", film['title'])
+
+for film in films:
+    save(film)
